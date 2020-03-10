4893fee2f440b86af8acafc3aa0b880e81f79d6d
gortron <gordon.lanza@gmail.com>
2020-03-09 18:43:06 -0700

    WIP on google maps minimum viable component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import SimpleMap from "./components/Map";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SimpleMap center={{ lat: 37.38, lng: 122.23 }} zoom={{ zoom: 11 }} />
     </div>
   );
 }
